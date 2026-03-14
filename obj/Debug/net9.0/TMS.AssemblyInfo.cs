95a98f6c2922c8c13884769eefa9e7750bf044bf
ehsanahmad <ea973220@gmail.com>
2026-03-14 21:59:34 +0500

    Update AssemblyInformationalVersion and cache inputs

--- obj/Debug/net9.0/TMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/TMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e27ae5a283826332df1176f5eb4b69f9b2de68b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc777c6696ed624d24068a22143c42eef9a6675")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
